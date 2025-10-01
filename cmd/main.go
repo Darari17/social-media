7597392610a6495413e1a16e547a240f99fd247d
farid <faridramadhan59@gmail.com>
2025-10-01 11:29:55 +0700

    feat: add main function and go modules

---
+++ cmd/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"log"
+
+	"github.com/Darari17/social-media/internal/configs"
+	"github.com/Darari17/social-media/internal/routers"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	// load env
+	if err := godotenv.Load(); err != nil {
+		log.Println("Failed to load env.\nCause:", err.Error())
+		return
+	}
+
+	// init db
+	db, err := configs.InitDB()
+	if err != nil {
+		log.Println("Failed to connect to database.\nCause:", err.Error())
+		return
+	}
+	log.Println("DB Connected")
+	defer db.Close()
+
+	// test db
+	if err := configs.PingDB(db); err != nil {
+		log.Println("Ping to DB failed.\nCause:", err.Error())
+		return
+	}
+
+	// init redis
+	rdb, err := configs.InitRedis()
+	if err != nil {
+		log.Println("Failed to connect Redis.\nCause: ", err.Error())
+		return
+	}
+	log.Println("Redis Connected.")
+	defer rdb.Close()
+
+	// router
+	router := routers.InitRouter(db, rdb)
+	router.Run(":8080")
+}
