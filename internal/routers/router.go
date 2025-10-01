34891a1b83d9098223da055209d04feafe340062
farid <faridramadhan59@gmail.com>
2025-10-01 14:28:05 +0700

    feat: add swagger route

--- internal/routers/router.go
+++ internal/routers/router.go
@@ -3,10 +3,14 @@ package routers
 import (
 	"net/http"
 
+	"github.com/Darari17/social-media/docs"
 	"github.com/Darari17/social-media/internal/dtos"
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/redis/go-redis/v9"
+
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func InitRouter(db *pgxpool.Pool, rdb *redis.Client) *gin.Engine {
@@ -17,8 +21,12 @@ func InitRouter(db *pgxpool.Pool, rdb *redis.Client) *gin.Engine {
 	InitPostRouter(r, db, rdb)
 	InitFollowRouter(r, db, rdb)
 	InitLikeRoutes(r, db, rdb)
+	InitCommentRouter(r, db, rdb)
 
 	r.Static("/img", "public")
+
+	docs.SwaggerInfo.BasePath = "/"
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 
 	r.NoRoute(func(ctx *gin.Context) {
 		ctx.JSON(http.StatusNotFound, dtos.Response{
